2430415bfba4ed651278f55a08b06fe20d9b76dc
Julian Dühnen (s2219735) <j.b.duhnen@student.utwente.nl>
2024-06-26 00:15:52 +0200

    Using tasks

--- src/commands/projects.runCsynth.ts
+++ src/commands/projects.runCsynth.ts
@@ -6,6 +6,11 @@ import { OutputConsole } from '../outputConsole';
 import { SolutionInfo } from "../projectManager";
 
 export default async (solution: SolutionInfo) => {
+    if (vscode.tasks.taskExecutions.some(value => value.task.source === 'Vitis HLS IDE')) {
+        vscode.window.showErrorMessage('A task is already running. Please wait for it to finish before running another one.');
+        return;
+    }
+
     const vitisPath = vscode.workspace.getConfiguration('vitis-hls-ide')?.get<string>('vitisPath');
 
     if (!vitisPath) {
@@ -13,7 +18,7 @@ export default async (solution: SolutionInfo) => {
         return;
     }
 
-    OutputConsole.instance.appendLine('Running C Synthesis...');
+    OutputConsole.instance.appendLine('Running C synthesis...');
     OutputConsole.instance.show();
 
     const tclContent =
@@ -31,22 +36,48 @@ export default async (solution: SolutionInfo) => {
             return;
         }
 
-        const terminal = vscode.window.createTerminal('Run C Synthesis');
-        terminal.sendText(`cd ${path.join(solution.project.path, "..")} && cmd /c "${path.join(vitisPath, "bin", "vitis-run")} --mode hls --tcl ${tclFilePath}"`);
-        terminal.show();
+        const task = new vscode.Task(
+            { type: 'shell' },
+            vscode.TaskScope.Workspace,
+            'Run C synthesis',
+            'Vitis HLS IDE',
+            new vscode.ShellExecution(`cd ${path.join(solution.project.path, "..")} && cmd /c "${path.join(vitisPath, "bin", "vitis-run")} --mode hls --tcl ${tclFilePath}"`),
+            [],
+        );
 
-        const filePathToWatch = path.join(solution.project.path, solution.name, `${solution.name}.log`);
-        let watcher = vscode.workspace.createFileSystemWatcher(filePathToWatch);
+        task.presentationOptions = {
+            reveal: vscode.TaskRevealKind.Always,
+            panel: vscode.TaskPanelKind.Shared,
+            showReuseMessage: true,
+            clear: true,
+        };
 
-        // TODO Potential memory leak here, need to dispose the watcher and terminal
-        let disposable = watcher.onDidChange(async () => {
-            await vscode.workspace.fs.readFile(vscode.Uri.file(filePathToWatch)).then(content => {
-                OutputConsole.instance.appendLine(content.toString());
-            });
-            terminal.dispose();
-            disposable.dispose();
-            OutputConsole.instance.show();
-            OutputConsole.instance.appendLine('C Synthesis finished');
+        vscode.tasks.executeTask(task);
+
+        const taskEndListener = vscode.tasks.onDidEndTaskProcess(async e => {
+            if (e.execution.task.name === task.name) {
+                taskEndListener.dispose();
+
+                fs.unlink(tclFilePath, (err) => {
+                    if (err) {
+                        vscode.window.showErrorMessage(`Error deleting tcl file for C synthesis: ${err.message}`);
+                    }
+                });
+
+                const filePathToWatch = path.join(solution.project.path, solution.name, `${solution.name}.log`);
+
+                await vscode.workspace.fs.readFile(vscode.Uri.file(filePathToWatch)).then(content => {
+                    OutputConsole.instance.appendLine(content.toString());
+                    OutputConsole.instance.show();
+                });
+
+                if (e.exitCode !== 0) {
+                    OutputConsole.instance.appendLine('C synthesis failed');
+                    return;
+                } else {
+                    OutputConsole.instance.appendLine('C synthesis finished');
+                }
+            }
         });
     });
 };
